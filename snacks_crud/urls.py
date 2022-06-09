c909a1836ade8a3dca56ac141aee3a2f98ace216
Emily Landers <emily.j.landers@gmail.com>
2022-06-08 20:49:09 -0400

    fixes minor issues

--- snacks_crud/urls.py
+++ snacks_crud/urls.py
@@ -1,15 +1,23 @@
-from django.urls import path
 
-from .views import SnackListView, SnackDetailView, SnackCreateView, SnackUpdateView, SnackDeleteView
-
-from django.conf import settings
-from django.conf.urls.static import static
+   
+"""snacks_crud URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', SnackListView.as_view(), name='snack_list'),
-    path('<int:pk>', SnackDetailView.as_view(), name='snack_detail'),  # Integer, Primary Key
-    path('create/', SnackCreateView.as_view(), name='snack_create'),
-    path('<int:pk>/update/', SnackUpdateView.as_view(), name='snack_update'),
-    path('<int:pk>/delete/', SnackDeleteView.as_view(), name='snack_delete'),
-
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('snacks.urls')),  # includes('app name .urls'))
+]
